919f48396ebbfdde4b969f5869063fbe14982538
Ian <ianluong@hotmail.co.uk>
2022-09-30 22:24:08 +0100

    Add MERN playlist files

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+
+const express = require("express")
+const mongoose = require("mongoose")
+const workoutRoutes = require("./routes/workouts")
+
+//EXPRESS APP
+const app = express()
+
+//MIDDLEWARE
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+//ROUTERS
+app.use("/api/workouts/", workoutRoutes)
+
+// CONNECT TO DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //SETUP APP ONCE DB HAS LOADED
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}...`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
