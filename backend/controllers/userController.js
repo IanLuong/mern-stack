e495e94da3b568e74f32bc995ef50841d901ab57
Ian <ianluong@hotmail.co.uk>
2022-10-03 13:06:07 +0100

    Add login authentication with jwt tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,24 @@
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
+
+//create a user web token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
+}
 
 //login
 const loginUser = async (req, res) => {
-  res.json({ message: "login user" })
+  const { email, password } = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 //signup
@@ -11,7 +27,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password)
 
-    res.status(200).json({ email, user })
+    //create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
   } catch (err) {
     res.status(400).json({ error: err.message })
   }
